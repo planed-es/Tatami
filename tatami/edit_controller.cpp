a95afa2b84ff1fb5ad1d321aadcf6c8dcaff1435
plaristote <michael@unetresgrossebite.com>
2022-10-21 21:07:44 +0200

    adds TatamiEditController

---
+++ tatami/edit_controller.cpp
@@ -0,0 +1,91 @@
+#include <QDebug>
+#include "edit_controller.h"
+
+std::function<void()> jsValueToCallback(QJSValue callback)
+{
+  if (callback.isCallable())
+  {
+    return [callback]()
+    {
+      QJSValue mutable_callback(callback);
+
+      if (callback.isCallable())
+        mutable_callback.call();
+    };
+  }
+  return std::function<void()>();
+}
+
+TatamiEditController::TatamiEditController(QObject *parent) : TatamiController(parent)
+{
+  connect(this, &TatamiEditController::sampleChanged, this, &TatamiEditController::connectSample);
+  connect(&displayService(), &DisplayService::invalidModel, this, &TatamiEditController::invalidModel);
+}
+
+void TatamiEditController::connectSample()
+{
+  if (sample)
+  {
+    connect(sample, &MetaRecordNotifiable::attributeChanged, this, &TatamiEditController::sampleAttributesChanged);
+    decorateSample(sample);
+  }
+}
+
+TatamiController::ModelType* TatamiEditController::startCreation()
+{
+  auto* result = displayService().startCreation();
+
+  if (result)
+  {
+    sample = result;
+    setCurrentItem(sample);
+    emit sampleChanged();
+    getTable()->setState(TatamiTableModel::CreateState);
+  }
+  return sample;
+}
+
+TatamiController::ModelType* TatamiEditController::startEdition(const ModelType* editedModel)
+{
+  if (editedModel && displayService().values().indexOf(const_cast<ModelType*>(editedModel)) >= 0)
+  {
+    sample = displayService().startEdition(*editedModel);
+
+    setCurrentItem(editedModel);
+    emit sampleChanged();
+    getTable()->setState(TatamiTableModel::UpdateState);
+    return sample;
+  }
+  return nullptr;
+}
+
+void TatamiEditController::endEdition(bool save, QJSValue jsCallback)
+{
+  auto callback = std::bind(&TatamiEditController::callbackWithIndexUpdate, this, currentIndex(), jsCallback);
+
+  displayService().endEdition(save, [this, callback]()
+  {
+    if (sample)
+      disconnect(sample, &MetaRecordNotifiable::attributeChanged, this, &TatamiEditController::sampleAttributesChanged);
+    sample = nullptr;
+    getTable()->setState(TatamiTableModel::ReadState);
+    emit sampleChanged();
+    callback();
+  });
+}
+
+void TatamiEditController::remove(ModelType* model, QJSValue jsCallback)
+{
+  auto callback = std::bind(&TatamiEditController::callbackWithIndexUpdate, this, currentIndex(), jsCallback);
+
+  displayService().destroy(model, callback);
+}
+
+void TatamiEditController::callbackWithIndexUpdate(int index, QJSValue jsCallback)
+{
+  auto callback = jsValueToCallback(jsCallback);
+
+  if (count() <= index) setCurrentIndex(count() - 1);
+  else                  setCurrentIndex(index);
+  if (callback) callback();
+}
---
+++ tatami/edit_controller.h
@@ -0,0 +1,42 @@
+#ifndef  SAMPLECONTROLLERCOMPONENT_H
+# define SAMPLECONTROLLERCOMPONENT_H
+
+# include "controller.h"
+
+class TATAMI_EXPORT TatamiEditController : public TatamiController
+{
+  Q_OBJECT
+
+  Q_PROPERTY(MetaRecordable* sample READ getSample NOTIFY sampleChanged)
+public:
+
+  explicit TatamiEditController(QObject* parent = nullptr);
+
+  ModelType* getSample() const { return sample; }
+
+  template<typename MODEL>
+  MODEL* sampleAs() const { return reinterpret_cast<MODEL*>(sample); }
+
+  Q_INVOKABLE virtual ModelType* startEdition(const ModelType*);
+  Q_INVOKABLE virtual ModelType* startCreation();
+  Q_INVOKABLE virtual void endEdition(bool save, QJSValue callback = QJSValue());
+  Q_INVOKABLE virtual void remove(ModelType*, QJSValue callback = QJSValue());
+
+signals:
+  void sampleChanged();
+  void sampleAttributesChanged();
+  void invalidModel(const ModelType*);
+
+private slots:
+  void connectSample();
+
+protected slots:
+  virtual void decorateSample(ModelType*) const {}
+
+protected:
+  void callbackWithIndexUpdate(int lastIndex, QJSValue callback);
+
+  ModelType* sample = nullptr;
+};
+
+#endif // SAMPLECONTROLLERCOMPONENT_H
